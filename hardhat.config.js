a4eaae11bacf7c42c5065ef3bdf1d97a95e37a98
miguelleonmarti <miguelleonmarti@gmail.com>
2022-06-18 11:02:15 +0100

    chore: hardhat config updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,12 +12,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.7",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 };
