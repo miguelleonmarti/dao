02c08021fd58fc30520c588d5a97bb893b0068b5
miguelleonmarti <miguelleonmarti@gmail.com>
2022-06-19 20:33:46 +0100

    chore: added moralis packages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={"jA8UpaTkf04VWM7OJtpRkmDeWmLMuJrV2q6smoQF"} serverUrl={"https://d3ncibarfsf6.usemoralis.com:2053/server"}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
